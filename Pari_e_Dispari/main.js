0cfbee4592f658fffeff376061650500db4fadf8
JennyPersichini <nenny1096@gmail.com>
2023-04-05 15:13:43 +0200

    1st step

---
+++ Pari_e_Dispari/main.js
@@ -0,0 +1,8 @@
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+let pariDispariChoice = prompt("Inserisci 'pari' o 'dispari'");
+let numChoice = prompt("Inserisci un numero tra 1 e 5");
+let num1 = parseInt(numChoice);
+
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
