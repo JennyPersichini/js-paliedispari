cdd76f67c5704a7f35ccc9f83e9d4f19616d8601
JennyPersichini <nenny1096@gmail.com>
2023-04-05 13:24:37 +0200

    write guideline

---
+++ Palindroma/main.js
@@ -0,0 +1,9 @@
+//Chiedere all’utente di inserire una parola.
+
+let word = prompt("Inserire una parola");
+
+//Creare una funzione per capire se la parola inserita è palindroma.
+
+//Palindroma: è una parola che letta in entrambi i versi, da la stessa parola.
+//La parola inserita si può leggere da sx verso dx??? E da dx verso sx??? Se si è PALINDROMA!!
+
