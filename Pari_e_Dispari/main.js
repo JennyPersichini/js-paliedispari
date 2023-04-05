23d52c3f094dfd84a58be75825b273123f4b22ae
JennyPersichini <nenny1096@gmail.com>
2023-04-05 15:32:50 +0200

    2nd step

--- Pari_e_Dispari/main.js
+++ Pari_e_Dispari/main.js
@@ -4,5 +4,13 @@ let pariDispariChoice = prompt("Inserisci 'pari' o 'dispari'");
 let numChoice = prompt("Inserisci un numero tra 1 e 5");
 let num1 = parseInt(numChoice);
 
+console.log(`Il numero da te scelto è ${num1}`)
+
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
+function numeComputer(){
+    num2 = Math.floor(Math.random() * 5) + 1;
+    console.log(`Il computer ha scelto ${num2}`);
+}
+
+//Dichiariamo chi ha vinto.
